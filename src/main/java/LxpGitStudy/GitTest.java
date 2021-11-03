95a7d9acb664938f5b70d99b8964ffb805ae27cb
lxp <2675088487@qq.com>
2021-11-03 12:21:16 +0800

    Second commit

--- src/main/java/LxpGitStudy/GitTest.java
+++ src/main/java/LxpGitStudy/GitTest.java
@@ -3,5 +3,6 @@ package LxpGitStudy;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("lxp hello git");
+        System.out.println("Second commit");
     }
 }
